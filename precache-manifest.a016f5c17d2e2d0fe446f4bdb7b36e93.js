92ba8d9a40bdfa6f89941505e9d6d2f91364b6d2
Saurabh Shekhar (HCL America Inc) <v-saushe@microsoft.com>
2020-02-28 13:10:46 +0530

    Updates

---
+++ precache-manifest.a016f5c17d2e2d0fe446f4bdb7b36e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d8ffd88a8214f95a6d2bf802ff99f3",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "db1f4bd08c92a216d91f",
+    "url": "/shopping-cart/static/css/2.765f46be.chunk.css"
+  },
+  {
+    "revision": "9728eba5687159901711",
+    "url": "/shopping-cart/static/css/main.4eee2696.chunk.css"
+  },
+  {
+    "revision": "db1f4bd08c92a216d91f",
+    "url": "/shopping-cart/static/js/2.69d1314e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/shopping-cart/static/js/2.69d1314e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9728eba5687159901711",
+    "url": "/shopping-cart/static/js/main.c7f23969.chunk.js"
+  },
+  {
+    "revision": "076e268f506dc96e7a27",
+    "url": "/shopping-cart/static/js/runtime-main.18f3f39c.js"
+  }
+]);
